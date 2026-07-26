c840d63a72f259bda48a803a5e4ca72a744c28a9
YvlLima <goncalomartinslima2007@gmail.com>
2026-07-26 17:15:02 +0100

    fix: atualizar URL da API para o Render

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://musichub-backend-bf0h.onrender.com";
 
 // ==========================================
 // 1. UTILITÁRIOS E FEEDBACK DA INTERFACE
